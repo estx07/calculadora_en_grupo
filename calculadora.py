51650f5bde65d56f02f253ca8e911554ecb35a14
estx07 <87713240+estx07@users.noreply.github.com>
2022-06-06 21:42:12 -0300

    Iniciado fichero base de la actividad.

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+#Ejercicio Grupal
+
+""" 
+Crear un programa en Python que tendrá 3 o 4 funciones:
+
+* Suma, 3 parámetros, devuelve la suma de los 3.
+* Resta, 2 parámetros, devuelve la resta de los 2.
+* Producto, 4 parámetros, devuelve el producto de los 4.
+* Imprimir, 2 parámetros, texto y valor, devuelve la impresión de los valores pasados.
+Usaremos Visual Studio Code, con la extensión de Python para hacer la práctica.
+El equipo crea un Project en Githug y agrega 2 al menos 2 issue por cada tarea a realizar.
+Crear una rama por cada miembro. 
+
+"""
